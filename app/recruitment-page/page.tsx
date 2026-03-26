a2c197f37b6ff70a18b6a00eea27be1accf08497
Quan <152340025+quanhuynh11@users.noreply.github.com>
2026-03-26 13:23:06 -0600

    start on page

---
+++ app/recruitment-page/page.tsx
@@ -0,0 +1,9 @@
+export default function RecruitmentPage() {
+    return (
+        <div className="p-5 flex justify-center bg-neutral-800 w-screen h-screen">
+            <div className="flex flex-col">
+                <h1 className="text-5xl mb-5">Recruitment Page</h1>
+            </div>
+        </div>
+    )
+}
